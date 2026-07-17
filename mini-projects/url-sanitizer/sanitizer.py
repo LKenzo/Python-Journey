25d48ed7e870398d0ce422fd1b2ef3dcf6753f35
Maximillian <maxdvega@gmail.com>
2026-07-17 21:28:18 +0700

    fix: modified the validation of url after sanitizing, add TLD list checking

--- mini-projects/url-sanitizer/sanitizer.py
+++ mini-projects/url-sanitizer/sanitizer.py
@@ -1,9 +1,8 @@
 def main():
-    sanitized_url = url_sanitizer(input("Input the URL: ").strip)
-    print(f"Sanitized Url: {sanitized_url}")
+    sanitized_url = url_sanitizer(input("Input the URL: ").strip())
+    input_to_file(sanitized_url)
 
 def url_sanitizer(url):
-
     if url.startswith(("http", "https")):
         url = url.removeprefix("https://").removeprefix("http://")
     if url.startswith("www"):
@@ -13,12 +12,27 @@ def url_sanitizer(url):
 
     if(not url_is_valid(url)):
         raise ValueError("Value is not valid")
-    
-    return url 
+
+    return url
 
 def url_is_valid(url):
-    return True if "." in url else False
+    if " " in url:
+        return False
+    
+    splitted_url = url.split(".")
+    length = len(splitted_url) - 1
+    with open("tld_list.txt") as file:
+        for valid_tld in file:
+            if splitted_url[length] == valid_tld.strip():
+                return True
+            
+    return False
 
+def input_to_file(url):
+    with open("sanitized_url.txt", "a") as file:
+        url = url + "\n"
+        file.write(url)
+        print("URL Input Success")
 
 if __name__ == "__main__":
     main()
--- mini-projects/url-sanitizer/test_sanitizer.py
+++ mini-projects/url-sanitizer/test_sanitizer.py
@@ -6,3 +6,18 @@ def test_url():
     
     with pytest.raises(ValueError):
         url_sanitizer("notvalidurl")
+
+def test_dot_url():
+    with pytest.raises(ValueError):
+        url_sanitizer("www.youtube.zzz")
+    
+    with pytest.raises(ValueError):
+        url_sanitizer("roblox.")
+
+    with pytest.raises(ValueError):
+        url_sanitizer("google    .com")
+
+    with pytest.raises(ValueError):
+        url_sanitizer("non://google    .")
+
+    
